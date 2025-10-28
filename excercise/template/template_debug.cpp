a64ae7e06aefae4c451c5e20de36b611c58eb496
Wistfy <huy.vd76@gmail.com>
2025-10-28 23:09:37 +0700

    Init prj

---
+++ excercise/template/template_debug.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+#define dbg(x) cerr << #x << " = " << (x) << endl;
+#define dbg_vec(v) cerr << #v << " = "; for (auto x : v) cerr << x << " "; cerr << endl;
+#define dbg_map(m) cerr << #m << ":\n"; for (auto x : m) cerr << x.first << " -> " << x.second << endl;
+
+void solve(){}
+
+
+int main() {
+    solve();
+    return 0;
+}
